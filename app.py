8c451567361c2c1d5dc1b1626229f774880dbabf
Glitch (glitch-hello-website) <none>
2022-12-30 12:38:30 +0000

    🐾🕹 Checkpoint ./templates/index.html:31660220/419 ./app.py:31660220/22

--- app.py
+++ app.py
@@ -8,8 +8,7 @@ app = Flask(__name__)
 @app.route('/index') #not sure why i have to add this to get it to readd my index.html file correctly.
 def index():
     list_of_passwords = "List of Passwords Here!"
-    return render_template('index.html', title='Password Generator', password_list=list_of_passwords)
-
+    return render_template('index.html', title='Password Generator v2.0', password_list=list_of_passwords)
 
 if __name__ == '__main__':
     app.run()
