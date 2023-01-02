c169ba42db6ede13e0a0a2c3f0e695208e86a475
Glitch (glitch-hello-website) <none>
2023-01-02 11:41:33 +0000

    🐕🍜 Checkpoint ./app.py:31660220/829 ./templates/index.html:31660220/641

--- app.py
+++ app.py
@@ -36,21 +36,13 @@ def passwordGenerator():
   finalPassword = ''.join(myPassword)
   return finalPassword
 
-#adding in a separate function to create multiple passwords
-def passwordAggregator():
-  password_1 = passwordGenerator()
-  password_2 = passwordGenerator()
-  password_3 = passwordGenerator()
-  password_4 = passwordGenerator()
-  password_5 = passwordGenerator()
-  combinedPassword = password_1 + "\n" + password_2 + "\n" + password_3 + "\n" + password_4 + "\n" + password_5
-  return combinedPassword
-
 @app.route('/') #if i remove this - it stops working
 @app.route('/index') #not sure why i have to add this to get it to readd my index.html file correctly.
 def index():
     list_of_passwords = "List of Passwords Here!"
-    return render_template('index.html', title='Password Generator v2.0', password_1=passwordGenerator())
-
+    return render_template('index.html', title='Password Generator v2.0', password_1=passwordGenerator(), password_2=passwordGenerator(), password_3=passwordGenerator(), password_4=passwordGenerator(), password_5=passwordGenerator())
+#to get the html template to print the passwords on different lines, i had to create 5 different variables that ran the password generator function multiple times.
+#there might be a way to do this cleaner
+  
 if __name__ == '__main__':
     app.run()
